9f898544a751dc8e611d6f3be8be019a7c0286f5
dito <dr.dhitz@gmail.com>
2019-02-16 09:13:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,21 +15,22 @@ $hasil = mysqli_query($mysqli, "SELECT * FROM tabel_user ORDER BY id DESC");
 <a href="tambah.php">Tambah User Baru</a><br/><br/>
     <table width='80%' border=1>
     <tr>
-        <th>Nama</th> <th>Mobile</th> <th>Email</th> <th>Alamat</th> <th>Update</th>
+        <th>Foto</th> <th>Nama</th> <th>Mobile</th> <th>Email</th> <th>Alamat</th> <th>Update</th>
     </tr>
     <?php  
     while($user_data = mysqli_fetch_array($hasil)) {   
 	//while merupakan perulangan yang akan mengulang nama variable dari user_data dimana memiliki nilai yaitu variable hasil.
         echo "<tr>";
-        echo "<td>".$user_data['nama']."</td>"; 	
+        echo "<td align='center'><img src='gambar/".$user_data['foto']."' width='80' height='80'></td>";
+	echo "<td align='center'>".$user_data['nama']."</td>"; 	
 					//user_data merupakan variable yang akan memanggil nama kolom yang ada didatabase yaitu nama
-        echo "<td>".$user_data['mobile']."</td>"; 	
+        echo "<td align='center'>".$user_data['mobile']."</td>"; 	
 					//user_data merupakan variable yang akan memanggil nama kolom yang ada didatabase yaitu mobile
-        echo "<td>".$user_data['email']."</td>";    
+        echo "<td align='center'>".$user_data['email']."</td>";    
 					//user_data merupakan variable yang akan memanggil nama kolom yang ada didatabase yaitu email
-		echo "<td>".$user_data['alamat']."</td>"; 
-					//user_data merupakan variable yang akan memanggil nama kolom yang ada didatabase yaitu email
-		echo "<td><a href='edit.php?id=$user_data[id]'>Edit</a></td></tr>";   
+	echo "<td align='center'>".$user_data['alamat']."</td>"; 
+					//user_data merupakan variable yang akan memanggil nama kolom yang ada didatabase yaitu email	
+	echo "<td align='center'><a href='edit.php?id=$user_data[id]'>Edit</a> | <a href='delete.php?id=$user_data[id]'>Delete</a></td></tr>";   
     }
     ?>
     </table>
